18b89e72acc6496d58d3c2f198e80414dddf69f3
Daniel <djperrefort@gmail.com>
2023-02-27 08:46:39 -0500

    Replace original development version with production version

--- shinigami.py
+++ shinigami.py
@@ -1,41 +1,45 @@
 #!/usr/bin/env python
-'''
+"""
 Written By:     Barry Moore II
 Purpose:        Kill errant processes on compute nodes. Slurm doesn't clean up well after itself.
 Name Origin:    Shinigami are gods or supernatural spirits which invite humans to death.
-'''
-from subprocess import Popen, PIPE
-from shlex import split
+"""
+
 from datetime import datetime
+from shlex import split
+from subprocess import Popen, PIPE
+
+# The clusters we want to check for dead processes
+clusters = ["gpu", "mpi", "invest"]
+admin_users = ['leb140', 'djp81', 'nlc60', 'chx33', 'yak73', 'kimwong', 'sak236', 'jar7', 'twc17', 'fangping', 'gam134']
+log_directory = '/zfs1/crc/logs/shinigamit'  # No trailing slash
 
 
 def run_command_to_list(command):
-    sp = Popen(split(command), stdout=PIPE, stderr=PIPE)
-    return sp.communicate()[0].strip().split('\n')
+    """Run a shell command and return STDOUT as a list
 
+    Args:
+        command: The command to run
 
-def run_command(command):
-    sp = Popen(split(command), stdout=PIPE, stderr=PIPE)
-    return sp.communicate()
+    Returns:
+        A list of lines written to STDOUT by the command
+    """
 
+    sub_proc = Popen(split(command), stdout=PIPE, stderr=PIPE)
+    return sub_proc.communicate()[0].strip().split('\n')
 
-# The clusters we want to check for dead processes
-clusters = ["smp", "gpu", "mpi", "invest"]
 
 # Figure out which nodes are active
 nodelist = {}
 for cluster in clusters:
-    print_nodelist = True
-    nodes = run_command_to_list("sinfo -M {0} -t mix,alloc,idle -N -o %N -h".format(cluster))[1:]
+    nodelist[cluster] = []
+    nodes = run_command_to_list("sinfo -M {0} -t mix,alloc,idle -N -o %N -h".format(cluster))
     for node in nodes:
-        if not node == '':
-            if print_nodelist:
-                nodelist[cluster] = []
-                print_nodelist = False
-            nodelist[cluster].append(node.strip())
+        node_name = node.strip()
+        if node != '' and node_name not in nodelist[cluster]:
+            nodelist[cluster].append(node_name)
 
-# `nodelist` now contains dictionary key: cluster, value: list of nodes in
-#   mix,alloc,idle states
+# `nodelist` now contains dictionary key: cluster, value: list of nodes in mix,alloc,idle states
 # for each node:
 # -> check node for users running jobs
 # -> check node for users running processes
@@ -43,13 +47,18 @@ for cluster in clusters:
 for cluster in nodelist.keys():
     for node in nodelist[cluster]:
         # Reset to_log/admin_log strings
-        to_log = ""
         admin_log = ""
-        
+
+        if "ppc-n" in node:
+            continue
+
+        if "mems-n" in node:
+            continue
+
         # Are there running jobs on this node?
-        slurm_jobs = run_command_to_list("squeue -h -M {0} -w {1} -o %A".format(cluster, node))[1:]
+        slurm_jobs = run_command_to_list("squeue -h -M {0} -w {1} -o %A".format(cluster, node))
         slurm_users = []
-        if len(slurm_jobs): # Running Jobs
+        if len(slurm_jobs):  # Running Jobs
             # Who is running the jobs?
             for job in slurm_jobs:
                 user = run_command_to_list("squeue -h -M {0} -w {1} -j {2} -o %u".format(cluster, node, job))[-1]
@@ -62,38 +71,43 @@ for cluster in nodelist.keys():
             sp = line.split()
             try:
                 pid, user, uid, time, cmd = int(sp[0]), sp[1], int(sp[2]), sp[3], sp[4:]
+
             except IndexError:
                 # Issue on the node, log it, move to the next
-                with open("/zfs1/crc/logs/shinigami/{0}.log".format(node), 'a') as log:
+                with open("{0}/{1}.log".format(log_directory, node), 'a') as log:
                     log.write("--> {0} <--\n".format(datetime.now()))
                     log.write("shinigami error")
+
                 continue
-            if uid >= 15000: # Probably need to add a whitelist here!
+
+            if uid >= 15000:  # Probably need to add a whitelist here!
                 if user not in proc_users:
                     proc_users.append((user, time, cmd, pid))
 
-        # slurm_users and proc_users contain non-root/service accounts                        
+        # slurm_users and proc_users contain non-root/service accounts
         # -> Are any users running processes, but not jobs?
         to_kill = {}
         for user, time, cmd, pid in proc_users:
-            if (user in ['bmooreii', 'shs159', 'kimwong', 'sak236', 'jar7', 'twc17', 'fangping']) and (user not in slurm_users):
+            if (user in admin_users) and (user not in slurm_users):
                 admin_log += "node: {0}, user: {1}, time: {2}, cmd: {3}, pid: {4}\n".format(node, user, time, cmd, pid)
+
             elif user not in slurm_users:
                 if user not in to_kill.keys():
                     to_kill[user] = [pid]
+
                 else:
                     to_kill[user].append(pid)
-                #to_log += "node: {0}, user: {1}, time: {2}, cmd: {3}, pid: {4}\n".format(node, user, time, cmd, pid)
 
         # Log information (if necessary)
         if to_kill:
-            with open("/zfs1/crc/logs/shinigami/{0}.log".format(node), 'a') as log:
+            with open("{0}/{1}.log".format(log_directory, node), 'a') as log:
                 log.write("--> {0} <--\n".format(datetime.now()))
                 for user, pids in to_kill.items():
                     kill_str = ' '.join([str(x) for x in pids])
-                    run_command("ssh {0} 'kill -9 {1}'".format(node, kill_str))
+                    run_command_to_list("ssh {0} 'kill -9 {1}'".format(node, kill_str))
                     log.write("User {0}, got `kill -9 {1}`".format(user, kill_str))
+
         if len(admin_log) != 0:
-            with open("/zfs1/crc/logs/shinigami/{0}-admin.log".format(node), 'a') as log:
+            with open("{0}/{1}-admin.log".format(log_directory, node), 'a') as log:
                 log.write("--> {0} <--\n".format(datetime.now()))
                 log.write("{0}".format(admin_log))
