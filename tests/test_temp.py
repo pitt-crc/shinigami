2f28321f829d91d10b34c93728e52b33f9d9dec0
Daniel <djperrefort@gmail.com>
2023-07-11 12:08:58 -0400

    Adds CI for running tests

---
+++ tests/test_temp.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+
+class ExampleTest(TestCase):
+    """These are placeholder tests"""
+
+    def test_something(self) -> None:
+        """This is an example test"""
