166886cec845b3eab9479c977fc47e15d194f1dc
Daniel <djperrefort@gmail.com>
2023-07-24 08:51:26 -0700

    Drops old tests

---
+++ tests/utils/test_placeholder.py
@@ -0,0 +1,12 @@
+"""This is a placeholder for future tests"""
+
+from unittest import TestCase
+
+
+class DummyTest(TestCase):
+    """A dummy test case"""
+
+    def test_something(self) -> None:
+        """Test a thing"""
+
+        pass
