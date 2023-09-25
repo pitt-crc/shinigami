d9e89b5753199a20fc54e1de96189c352884e878
Daniel Perrefort <djperrefort@gmail.com>
2023-09-25 11:11:43 -0400

    Fixes bug where whitelist can only contain one value

--- shinigami/settings.py
+++ shinigami/settings.py
@@ -20,7 +20,7 @@ class Settings(BaseSettings):
         default=False,
         description='When enabled, processes are scanned and logged but not terminated.')
 
-    uid_whitelist: Tuple[Union[int, Tuple[int, int]]] = Field(
+    uid_whitelist: Tuple[Union[int, Tuple[int, int]], ...] = Field(
         title='Whitelisted User IDs',
         default=(0,),
         description='Only terminate processes launched by users with the given UID values.')
